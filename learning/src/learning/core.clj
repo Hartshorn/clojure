422cba4125318a2e033bed91991cb70b90353d8f
michael <mpalmer16@gmail.com>
2016-03-20 15:43:40 -0500

    changes

--- learning/src/learning/core.clj
+++ learning/src/learning/core.clj
@@ -2,20 +2,20 @@
   (:gen-class))
 
 (defn rl []
-  ; convenience function to reload current file
+  "convenience function to reload current file"
   (use 'learning.core :reload-all))
 
 (defn mult-args
-  ; function that takes at least two arguments, ignoring the others
-  ; here c will be saved as a list (which could be any number of values)
+  "function that takes at least two arguments, ignoring the others
+  here c will be saved as a list (which could be any number of values)"
   [a b & c]
   (println (str "A and B: " a ", " b))
   (println (str "Ignoring: " c)))
 
 (defn go-to-location
-  ; function that takes a map and returns a message about how to
-  ; get there. dorun will show the side effects of a map over println
-  ; which would normally just return nil for each call
+  "function that takes a map and returns a message about how to
+  get there. dorun will show the side effects of a map over println
+  which would normally just return nil for each call"
   [{:keys [lat lng] :as location}]
   (println (str "HERE WE GO!"))
   (let [a (+ lat 10) b (+ lng 5)]
@@ -25,15 +25,15 @@
                          (str "Longitude: " (- b lng))]))))
 
 (defn location
-  ; function that takes a map and destructures it and saves it to pass
-  ; to another function
+  "function that takes a map and destructures it and saves it to pass
+  to another function"
   [{:keys [lat lng] :as lct}]
     (dorun (map println [(str "Latitude: " lat)
                          (str "Longitude: " lng)]))
     (go-to-location lct))
 
 (defn arity-example
-  ; a function that shows arity overloading
+  "a function that shows arity overloading"
   ([a b]
    (println (str "Arity of 2: " a " and " b)))
   ([a b c]
@@ -44,13 +44,13 @@
    (arity-example (:b a-and-b) 34)))
 
 (defn anon-func
-  ; demonstrate the use of anonymous functions in two differnent ways
+  "demonstrate the use of anonymous functions in two differnent ways"
   [a b]
   (println "Using fn syntax: " ((fn [x y] (+ x y 10)) a b))
   (println "Using #(%) syntax: " (#(+ %1 %2 10) a b)))
 
 (defn loop-example
-  ; function showing the use of loop and recur
+  "function showing the use of loop and recur"
   [howmany]
   (loop [increment 0]
     (println (str "Count: " increment "/" howmany))
@@ -59,14 +59,14 @@
       (recur (inc increment)))))
 
 (defn unique-vec-from-set
-  ; creates a unique vector of the items listed (no repeating values)
-  ; it also sorts them
+  "creates a unique vector of the items listed (no repeating values)
+  it also sorts them"
   [& values]
   (sort (into [] (set values))))
 
 (defn rand-samp-with-prob
-  ; gets the random sample of 10 numbers (1-10)
-  ; with p probability
+  "gets the random sample of 10 numbers (1-10)
+  with p probability"
   [p]
   (let [sample (repeatedly 10 #(rand-int 1000))]
     (loop [n 0]
@@ -77,14 +77,14 @@
           (recur (inc n)))))))
 
 (defn map-over-map
-  ; map over a map and do something to each value
-  ; (reduce (fn [m [k v]] (assoc m k (inc v))) {} {:some 1 :map 2})
+  "map over a map and do something to each value
+  (reduce (fn [m [k v]] (assoc m k (inc v))) {} {:some 1 :map 2})"
   [func map]
   (into {} (for [[key value] map] [key (func value)])))
 
 (defn my-count
-  ; a version of count - comes from 4clojure
-  ; #(loop [c 0 [h & t] %] (if (nil? t) (inc c) (recur (inc c) t)))
+  "a version of count - comes from 4clojure
+  #(loop [c 0 [h & t] %] (if (nil? t) (inc c) (recur (inc c) t)))"
   [seq]
   (loop [count 0 [first & rest] seq]
     (if (nil? rest)
@@ -92,13 +92,27 @@
       (recur (inc count) rest))))
 
 (defn my-reverse
-  ; a version of reverse - from 4clojure
-  ; #(loop [n '() [h & t] %] (if (nil? t) (cons h n) (recur (cons h n) t)))
+  "a version of reverse - from 4clojure
+  #(loop [n '() [h & t] %] (if (nil? t) (cons h n) (recur (cons h n) t)))"
   [s]
   (loop [n '() [h & t] s]
     (if (nil? t)
       (cons n h)
       (recur (cons n h) t))))
 
+; fibonacci stuff
+(defn fib
+  "creates the nth fibonacci number"
+  [n]
+  (if (or (= n 1) (= n 2))
+    1
+    (if (= n 0)
+      0
+      (+ (fib (- n 1)) (fib (- n 2))))))
+
+(defn n-fibs
+  "create a list of n fibonacci numbers"
+  [n]
+  (map fib (range n)))
 
 ; left off at http://www.braveclojure.com/core-functions-in-depth/ on filter and some (a little less than half way down!)
